930870f1b5cd0f4e7f9c913232d8e58bd8f3413c
Andrew Lloyd <andrew85.lloyd@gmail.com>
2022-03-05 19:35:36 -0800

    debug

--- test/test-integration.sh
+++ test/test-integration.sh
@@ -51,7 +51,10 @@ mv ${workDir}/pi-app-deployer-agent ${homeDir}
 ${homeDir}/pi-app-deployer-agent --app-user runneradmin --repo-name ${repo} --manifest-name ${manifestName} --install
 
 sleep 10
+systemctl is-active --quiet pi-app-deployer-agent.service
 systemctl status pi-app-deployer-agent.service
-systemctl status pi-test.service
 journalctl -u pi-app-deployer-agent.service
+
+systemctl is-active --quiet pi-test.service
+systemctl status pi-test.service
 journalctl -u pi-test.service
